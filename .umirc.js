3a67ec88a243baa9a8efbbbd828a825412558c9c
mukuashi <572512250@qq.com>
2018-08-25 13:27:26 +0800

    add scss check

--- .umirc.js
+++ .umirc.js
@@ -1,28 +1,30 @@
-import {resolve} from "path";
+import { resolve } from "path";
 //
 export default {
   // for query-string@6 https://github.com/sorrycc/blog/issues/68
   es5ImcompatibleVersions: true,
-  history: 'hash',
   plugins: [
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      routes: {
-        exclude: [
-          /model\.(j|t)sx?$/,
-          /service\.(j|t)sx?$/,
-          /models\//,
-          /components\//,
-          /services\//,
-        ],
-      },
-      dll: {
-        exclude: [],
-        include: ["dva", "dva/router", "dva/saga", "dva/fetch", "antd/es"],
-      },
-      hardSource: /* isMac */process.platform === 'darwin',
-    }],
+    [
+      "umi-plugin-react",
+      {
+        antd: true,
+        dva: true,
+        routes: {
+          exclude: [
+            /model\.(j|t)sx?$/,
+            /service\.(j|t)sx?$/,
+            /models\//,
+            /components\//,
+            /services\//
+          ]
+        },
+        dll: {
+          exclude: [],
+          include: ["dva", "dva/router", "dva/saga", "dva/fetch", "antd/es"]
+        },
+        hardSource: /* isMac */ process.platform === "darwin"
+      }
+    ]
   ],
   // theme: "./theme.config.js",
   // 接口代理示例
@@ -34,27 +36,26 @@ export default {
   //   },
   // },
   sass: {
-    'node-sass': true
+    "node-sass": true
   },
   alias: {
-    components: path.resolve(__dirname, 'src/components/'),
-    utils: path.resolve(__dirname, 'src/utils/'),
-    config: path.resolve(__dirname, 'src/config/'),
-    services: path.resolve(__dirname, 'src/services/'),
-    models: path.resolve(__dirname, 'src/models/'),
-    styles: path.resolve(__dirname, 'src/styles/'),
-    layouts: path.resolve(__dirname, 'src/layouts/'),
-    assets: path.resolve(__dirname, 'src/assets/'),
-    pages: path.resolve(__dirname, 'src/pages/')
+    components: resolve(__dirname, "src/components/"),
+    utils: resolve(__dirname, "src/utils/"),
+    config: resolve(__dirname, "src/config/"),
+    services: resolve(__dirname, "src/services/"),
+    models: resolve(__dirname, "src/models/"),
+    styles: resolve(__dirname, "src/styles/"),
+    layouts: resolve(__dirname, "src/layouts/"),
+    assets: resolve(__dirname, "src/assets/"),
+    pages: resolve(__dirname, "src/pages/")
   },
-  urlLoaderExcludes: [
-    /\.svg$/,
-  ],
+  urlLoaderExcludes: [/\.svg$/],
   ignoreMomentLocale: true,
   chainWebpack(config) {
-    config.module.rule('svg')
+    config.module
+      .rule("svg")
       .test(/\.svg$/i)
-      .use('svg-sprite-loader')
-      .loader(require.resolve('svg-sprite-loader'));
-  },
+      .use("svg-sprite-loader")
+      .loader(require.resolve("svg-sprite-loader"));
+  }
 };
